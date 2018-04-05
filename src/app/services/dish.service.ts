60b0b07b08c489e93a53ddf05b18a52ed6fae171
DJ Cyphers <djcyphers@gmail.com>
2018-04-04 20:55:16 -0700

    HTTP Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import { Http, Response } from '@angular/http';
 import { baseURL } from '../shared/baseurl';
 import { ProcessHTTPMsgService } from './process-httpmsg.service';
@@ -16,23 +16,27 @@ export class DishService {
   constructor(private http: Http,
     private processHTTPMsgService: ProcessHTTPMsgService) { }
 
-   getDishes(): Observable<Dish[]> {
+  getDishes(): Observable<Dish[]> {
     return this.http.get(baseURL + 'dishes')
-                    .map(res => { return this.processHTTPMsgService.extractData(res); });
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
   }
 
   getDish(id: number): Observable<Dish> {
     return  this.http.get(baseURL + 'dishes/'+ id)
-                    .map(res => { return this.processHTTPMsgService.extractData(res); });
+                    .map(res => { return this.processHTTPMsgService.extractData(res); })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
   }
 
   getFeaturedDish(): Observable<Dish> {
     return this.http.get(baseURL + 'dishes?featured=true')
-                    .map(res => { return this.processHTTPMsgService.extractData(res)[0]; });
+                    .map(res => { return this.processHTTPMsgService.extractData(res)[0]; })
+                    .catch(error => { return this.processHTTPMsgService.handleError(error); });
   }
 
-  getDishIds(): Observable<number[] | any> {
+  getDishIds(): Observable<number[] | any > {
     return this.getDishes()
-      .map(dishes => { return dishes.map(dish => dish.id) });
+      .map(dishes => { return dishes.map(dish => dish.id) })
+      .catch(error => { return error; } );
   }
 }
